489d9c7b39ac970a1efd4657352b1470cd6a58d0
rakiburrahman307 <rakiburrahman307@gmail.com>
2024-05-01 01:28:01 +0600

    add the navber section also a mobile resposive

--- src/Layout/Navbar/Navbar.jsx
+++ src/Layout/Navbar/Navbar.jsx
@@ -13,7 +13,9 @@ const Navbar = () => {
           Logo
         </h2>
         <div>
-          <h2 className='text-green-500 md:text-black text-lg font-semibold'>হাদিস সমূহ</h2>
+          <h2 className='text-green-500 md:text-black text-lg font-semibold'>
+            হাদিস সমূহ
+          </h2>
           <h4 className='hidden md:flex text-black text-xs font-light'>
             হাদিস পড়ুন শিখুন এবং জানুন
           </h4>
@@ -37,8 +39,9 @@ const Navbar = () => {
           <CiSearch className='text-2xl text-black' />
         </button>
         <div
-        onClick={() => setRightSidebar(true)} 
-        className='p-2 rounded-2xl bg-[#2B9E76] cursor-pointer group'>
+          onClick={() => setRightSidebar(true)}
+          className='p-2 rounded-2xl bg-[#2B9E76] cursor-pointer group'
+        >
           <IoIosSettings
             className='text-white group-hover:scale-110 duration-200'
             size={25}
@@ -66,7 +69,7 @@ const Navbar = () => {
               placeholder='Search Hadith'
               type='text'
             />
-            <div className="mt-5">
+            <div className='mt-5'>
               <form className='max-w-sm mx-auto'>
                 <label
                   htmlFor='small'
@@ -103,27 +106,71 @@ const Navbar = () => {
               </form>
             </div>
             {/* button type will be submit for handling form submission*/}
-           <div className="flex justify-between items-center gap-3">
-            <button
-              onClick={() => setOpenModal(false)}
-              className="rounded-lg bg-gray-300 px-8 py-3 text-xl font-light text-black duration-300 active:scale-95"
+            <div className='flex justify-between items-center gap-3'>
+              <button
+                onClick={() => setOpenModal(false)}
+                className='rounded-lg bg-gray-300 px-8 py-3 text-xl font-light text-black duration-300 active:scale-95'
               >
-              বাতিল
-            </button>
-           <button
-              className='rounded-lg bg-[#2B9E76] px-8 py-3 text-xl text-white duration-300 active:scale-95'
-            >
-              সার্চ করুন
-            </button>
-           </div>
+                বাতিল
+              </button>
+              <button className='rounded-lg bg-[#2B9E76] px-8 py-3 text-xl text-white duration-300 active:scale-95'>
+                সার্চ করুন
+              </button>
+            </div>
+
           </div>
         </div>
       </div>
-      <div className={`fixed z-[100] flex items-center justify-end ${openRightSidebar ? 'opacity-1 visible' : 'invisible opacity-0'} inset-0 bg-black/20 backdrop-blur-sm duration-100`}>
-          <div className={`absolute max-w-sm rounded-lg bg-white p-3 pb-5 text-center drop-shadow-2xl dark:bg-gray-800 dark:text-white ${openRightSidebar ? '-translate-x-100 opacity-1 duration-300' : 'translate-x-100 opacity-0 duration-150'} min-h-full`}>
-            <svg onClick={() => setRightSidebar(false)} className="mx-auto mr-0 w-8 cursor-pointer fill-black dark:fill-white" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g strokeWidth="0"></g><g strokeLinecap="round" strokeLinejoin="round"></g><g><path d="M6.99486 7.00636C6.60433 7.39689 6.60433 8.03005 6.99486 8.42058L10.58 12.0057L6.99486 15.5909C6.60433 15.9814 6.60433 16.6146 6.99486 17.0051C7.38538 17.3956 8.01855 17.3956 8.40907 17.0051L11.9942 13.4199L15.5794 17.0051C15.9699 17.3956 16.6031 17.3956 16.9936 17.0051C17.3841 16.6146 17.3841 15.9814 16.9936 15.5909L13.4084 12.0057L16.9936 8.42059C17.3841 8.03007 17.3841 7.3969 16.9936 7.00638C16.603 6.61585 15.9699 6.61585 15.5794 7.00638L11.9942 10.5915L8.40907 7.00636C8.01855 6.61584 7.38538 6.61584 6.99486 7.00636Z"></path></g></svg>
-          </div>
+
+      {/* right side menu ber modal */}
+      <div
+        className={`fixed z-[100] flex items-center justify-end ${
+          openRightSidebar ? "opacity-1 visible" : "invisible opacity-0"
+        } inset-0 bg-black/20 backdrop-blur-sm duration-100`}
+      >
+        <div
+          className={`absolute min-w-[350px] rounded-lg bg-white p-5 pb-5 text-center drop-shadow-2xl dark:bg-gray-800 dark:text-white ${
+            openRightSidebar
+              ? "-translate-x-100 opacity-1 duration-300"
+              : "translate-x-100 opacity-0 duration-150"
+          } min-h-full rounded-none`}
+        >
+       <div className="flex justify-between items-center gap-5 w-full">
+       <h4 className="text-xl font-semibold">সেটিংস</h4>
+          <svg
+            onClick={() => setRightSidebar(false)}
+            className='mx-auto mr-0 w-8 cursor-pointer fill-black dark:fill-white'
+            viewBox='0 0 24 24'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <g strokeWidth='0'></g>
+            <g strokeLinecap='round' strokeLinejoin='round'></g>
+            <g>
+              <path d='M6.99486 7.00636C6.60433 7.39689 6.60433 8.03005 6.99486 8.42058L10.58 12.0057L6.99486 15.5909C6.60433 15.9814 6.60433 16.6146 6.99486 17.0051C7.38538 17.3956 8.01855 17.3956 8.40907 17.0051L11.9942 13.4199L15.5794 17.0051C15.9699 17.3956 16.6031 17.3956 16.9936 17.0051C17.3841 16.6146 17.3841 15.9814 16.9936 15.5909L13.4084 12.0057L16.9936 8.42059C17.3841 8.03007 17.3841 7.3969 16.9936 7.00638C16.603 6.61585 15.9699 6.61585 15.5794 7.00638L11.9942 10.5915L8.40907 7.00636C8.01855 6.61584 7.38538 6.61584 6.99486 7.00636Z'></path>
+            </g>
+          </svg>
+       </div>
+       <div className="mt-14 w-full">
+       <label
+                  htmlFor='default'
+                  className='block mb-2 text-sm font-semibold text-left text-gray-900 dark:text-white'
+                >
+                  আরবি ফন্ট সিলেক্ট করুন
+                </label>
+                <select
+                  id='default'
+                  className='bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-white focus:border-white block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                >
+                  <option selected>Me Quran</option>
+                  <option value='Me Quran'>Me Quran</option>
+                  <option value='KFGQ'>KFGQ</option>
+                  <option value='Al Mushaf'>Al Mushaf</option>
+                  <option value='Amiri'>Amiri</option>
+                </select>
+       </div>
         </div>
+      </div>
     </nav>
   );
 };
